71c539bc606295f04b9d58411dc46921d4aa0b31
Bruno Silva Santana <ibrunoss@outlook.com>
2025-04-07 11:12:45 -0300

    feat(product): add getProductById to productDatabaseAdapter
    - Extend ProductAdapter interface to include method for fetching product by ID.
    - Implement getProductById in productDatabaseAdapter for retrieving product by ID.

--- adapters/product/database/product-database.adapter.ts
+++ adapters/product/database/product-database.adapter.ts
@@ -1,8 +1,10 @@
 import { getLatestProducts } from "@/adapters/product/database/get-latest-products";
 import { getProductBySlug } from "@/adapters/product/database/get-product-by-slug";
+import { getProductById } from "@/adapters/product/database/get-product-by-id";
 import { ProductAdapter } from "@/adapters/product/product.adapter";
 
 export const productDatabaseAdapter: ProductAdapter = {
   getLatestProducts,
   getProductBySlug,
+  getProductById,
 };
--- adapters/product/product.adapter.ts
+++ adapters/product/product.adapter.ts
@@ -3,4 +3,5 @@ import { ProductEntity } from "@/domain/product.entities";
 export interface ProductAdapter {
   getLatestProducts(): Promise<ProductEntity[]>;
   getProductBySlug(slug: string): Promise<ProductEntity | null>;
+  getProductById(id: string): Promise<ProductEntity | null>;
 }

701389d7160170ed9ef1163ac5f06996dca2d62f
Bruno Silva Santana <ibrunoss@outlook.com>
2025-04-07 11:01:38 -0300

    feat(cart): add getCartByUserId method to CartAdapter interface

--- adapters/cart/cart.adapter.ts
+++ adapters/cart/cart.adapter.ts
@@ -2,4 +2,5 @@ import { CartEntity } from "@/domain/cart.entities";
 
 export interface CartAdapter {
   getCartBySessionCartId(sessionCartId: string): Promise<CartEntity | null>;
+  getCartByUserId(userId: string): Promise<CartEntity | null>;
 }

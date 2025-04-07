44fbaf287f36086c1c89bb519e2791bac1b7e03e
Bruno Silva Santana <ibrunoss@outlook.com>
2025-04-07 11:14:11 -0300

    feat(cart): enhance addItemToCart with cart retrieval and product validation
    - Add getMyCart function to retrieve cart based on session or user ID.
    - Integrate product validation by fetching product details from the database.
    - Parse and validate cart item data before adding to cart.
    - Improve error handling with more specific action errors.

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -7,15 +7,32 @@ import {
   CathActionError,
   getActionErrors,
 } from "@/lib/actions/utils.actions";
-import { CartItemEntity } from "@/domain/cart.entities";
+import { CartEntity, CartItemEntity } from "@/domain/cart.entities";
+import { auth } from "@/auth";
+import { cartDatabaseAdapter } from "@/adapters/cart/database/cart-database.adapter";
+import { cartItemDatabaseSchema } from "@/lib/validators/cart";
+import { mapDomainCartItemToDatabaseCartItem } from "@/adapters/cart/database/map-domain-cart-item-to-database-cart-item";
+import { productDatabaseAdapter } from "@/adapters/product/database/product-database.adapter";
 
 export async function addItemToCart(
   prevState: ActionState,
   data: CartItemEntity
 ): Promise<ActionState> {
   try {
-    const sessionCartId = (await cookies()).get("sessionCartId")?.value;
-    console.log({ sessionCartId });
+    const cart = await getMyCart();
+
+    const item = cartItemDatabaseSchema.parse(
+      mapDomainCartItemToDatabaseCartItem(data)
+    );
+    const product = await productDatabaseAdapter.getProductById(item.productId);
+    // TESTING
+    console.log({
+      "Session Cart ID": cart?.id,
+      "User ID": cart?.userId,
+      "Item Requested": item,
+      "Item Found": product,
+    });
+
     return {
       success: true,
       message: `${data.name} adicionado ao carrinho`,
@@ -28,4 +45,26 @@ export async function addItemToCart(
 
     return getActionErrors({ error });
   }
+}
+
+export async function getMyCart(): Promise<CartEntity | undefined> {
+  // Check for cart cookie
+  const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+  if (!sessionCartId) {
+    throw new Error("Não foi possível encontrar o ID do carrinho na sessão");
+  }
+
+  // Get session and user ID
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  const cart = userId
+    ? await cartDatabaseAdapter.getCartByUserId(userId)
+    : await cartDatabaseAdapter.getCartBySessionCartId(sessionCartId);
+
+  if (!cart) {
+    return undefined;
+  }
+
+  return cart;
 }

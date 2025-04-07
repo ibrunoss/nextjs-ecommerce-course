777124410360c439ff0c0edf46fef6bbc40a57bb
Bruno Silva Santana <ibrunoss@outlook.com>
2025-04-07 11:02:05 -0300

    feat(cart): add getCartByUserId to cartDatabaseAdapter
    - Extend cartDatabaseAdapter to include method for fetching cart by user ID.

--- adapters/cart/database/cart-database.adapter.ts
+++ adapters/cart/database/cart-database.adapter.ts
@@ -1,6 +1,8 @@
 import { CartAdapter } from "@/adapters/cart/cart.adapter";
 import { getCartBySessionCartId } from "@/adapters/cart/database/get-cart-by-session-cart-id";
+import { getCartByUserId } from "@/adapters/cart/database/get-cart-by-user-id";
 
 export const cartDatabaseAdapter: CartAdapter = {
   getCartBySessionCartId,
+  getCartByUserId,
 };

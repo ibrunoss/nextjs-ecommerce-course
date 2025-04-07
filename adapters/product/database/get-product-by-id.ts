2c6b44e327720d0aef6d0dc10eab77d9b18ea8db
Bruno Silva Santana <ibrunoss@outlook.com>
2025-04-07 11:11:45 -0300

    feat(product): implement getProductById to fetch and map product data

---
+++ adapters/product/database/get-product-by-id.ts
@@ -0,0 +1,29 @@
+import { ProductEntity } from "@/domain/product.entities";
+import { getDatabaseProductById } from "@/infra/services/product/get-database-product-by-id";
+import { mapDatabaseProductToDomainProduct } from "@/adapters/product/database/map-database-product-to-domain-product";
+import { dateGenericAdapter } from "@/adapters/date/generic/date-generic.adapter";
+import { currencyGenericAdapter } from "@/adapters/currency/generic/currency.generic.adapter";
+
+export async function getProductById(
+  id: string
+): Promise<ProductEntity | null> {
+  const response = await getDatabaseProductById(id);
+  let product: ProductEntity | null;
+
+  if (!response) {
+    return null;
+  }
+
+  try {
+    product = mapDatabaseProductToDomainProduct(
+      response,
+      dateGenericAdapter,
+      currencyGenericAdapter
+    );
+  } catch (error) {
+    console.error("Error while mapping API product to domain product: ", error);
+    product = null;
+  }
+
+  return product;
+}

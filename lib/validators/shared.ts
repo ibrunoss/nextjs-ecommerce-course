aabf173625597635a5646e528561a8fdf87b1549
Bruno Silva Santana <ibrunoss@outlook.com>
2025-04-07 11:00:08 -0300

    fix(validation): remove unnecessary formatCurrency function in currencySchema

--- lib/validators/shared.ts
+++ lib/validators/shared.ts
@@ -1,11 +1,9 @@
 import { z } from "zod";
 
-import { formatCurrency } from "@/lib/utils";
-
 export const currencySchema = z
   .string()
   .min(0)
   .refine(
-    (value) => /^\d+(\.\d{2})?$/.test(formatCurrency(Number(value))),
+    (value) => /^\d+(\.\d{2})?$/.test(value),
     "O valor deve conter exatamente duas casas decimais"
   );

d5c0f5c0873617ef11b5f66c6a6cba9119497178
Bruno Silva Santana <ibrunoss@outlook.com>
2025-04-09 10:02:52 -0300

    refactor(routes): update product and cart paths, move routes to (shop) group
    - Changed product detail route from /produto/${slug} to /produto/${slug}/detalhes
    - Changed cart view route from /carrinho to /carrinho/visualizar
    - Moved relevant routes under the (shop) group
    - Updated route definitions in @/lib/constants/routes.ts:
      - Renamed `CART_PATH` to `CART_VIEW_PATH`
      - Updated `PRODUCT_DETAIL_PATH` to reflect the new structure
      - Updated `route` object keys accordingly

--- components/header/menu/cart-button.tsx
+++ components/header/menu/cart-button.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 import { ShoppingCart } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-import { CART_PATH } from "@/lib/constants/routes";
+import { CART_VIEW_PATH } from "@/lib/constants/routes";
 
 export const CartButton = () => {
   return (
     <Button asChild variant="ghost">
-      <Link href={CART_PATH}>
+      <Link href={CART_VIEW_PATH}>
         <ShoppingCart /> Carrinho
       </Link>
     </Button>
--- lib/constants/routes.ts
+++ lib/constants/routes.ts
@@ -1,14 +1,14 @@
 export const BASE_PATH = "";
 export const HOME_PATH = `${BASE_PATH}/`;
-export const CART_PATH = `${BASE_PATH}/carrinho`;
+export const CART_VIEW_PATH = `${BASE_PATH}/carrinho/visualizar`;
 export const PRODUCT_DETAIL_PATH = (slug: string) =>
-  `${BASE_PATH}/produto/${slug}`;
+  `${BASE_PATH}/produto/${slug}/detalhes`;
 export const SIGN_UP_PATH = `${BASE_PATH}/acesso/cadastrar`;
 export const SIGN_IN_PATH = `${BASE_PATH}/acesso/entrar`;
 
 export const route = {
   home: HOME_PATH,
-  cart: CART_PATH,
+  cartView: CART_VIEW_PATH,
   productDetail: PRODUCT_DETAIL_PATH,
   signUp: SIGN_UP_PATH,
   signIn: SIGN_IN_PATH,
